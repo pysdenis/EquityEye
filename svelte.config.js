ef60121a3ec5cf345e72daa3b2e8153892bbf3ae
Denis Pyš <128090772+pysdenis@users.noreply.github.com>
2024-11-01 00:14:17 +0100

    init

---
+++ svelte.config.js
@@ -0,0 +1,36 @@
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const postcssConfig = join(__dirname, 'postcss.config.js'); // https://stackoverflow.com/a/77137441
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: [
+		vitePreprocess(),
+		sveltePreprocess({
+			postcss: {
+				configFilePath: postcssConfig
+			}
+		})
+	],
+	kit: {
+		adapter: adapter({
+			out: '.build'
+		}),
+		appDir: 'src',
+	},
+	onwarn: (warning, handler) => {
+		const { code } = warning;
+		if (code === 'css-unused-selector') {
+			return;
+		}
+
+		handler?.(warning);
+	}
+};
+
+export default config;
