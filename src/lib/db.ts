916adcb9521db0631896c8cb7d8932eb18e1286d
Denis Pyš <denis.pys@seznam.cz>
2025-03-12 19:53:25 +0100

    wip

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,8 +5,6 @@ const connectDb = async () => {
 		if (mongoose.connection.readyState >= 1) return;
 
 		const dbUri = process.env.MONGO_URI;
-		console.log('dbUri:', dbUri);
-		console.log('Připojování k databázi MongoDB...');
 		if (!dbUri) {
 			console.error('Není nastavena proměnná prostředí MONGO_URI');
 			process.exit(1);
@@ -14,7 +12,6 @@ const connectDb = async () => {
 		await mongoose.connect(dbUri, {
 			dbName: 'equityeye',
 		});
-		console.log('Připojeno k databázi MongoDB');
 	} catch (error) {
 		console.error('Chyba při připojování k databázi MongoDB:', error);
 		process.exit(1);
