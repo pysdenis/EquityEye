723dbfde3be15b487d8cbd8c4b7f0f0fe41efbfc
Denis Pyš <denis.pys@seznam.cz>
2025-03-12 20:25:16 +0100

    wwwwF

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,18 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { sveltePreprocess } from 'svelte-preprocess';
 import 'dotenv/config';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const postcssConfig = join(__dirname, 'postcss.config.js');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-		sveltePreprocess({
-			postcss: {
-				configFilePath: postcssConfig
-			}
-		})
-	],
-	kit: {
-		adapter: adapter({
-			out: 'build'
-		}),
-	},
-	onwarn: (warning, handler) => {
-		const { code } = warning;
-		if (code === 'css-unused-selector') {
-			return;
-		}
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-		handler?.(warning);
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
